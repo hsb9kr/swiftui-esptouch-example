287a2afac18e6cab14e447aa04906208a1fa9597
Sang bo Hong <hsb9kr@gmail.com>
2021-05-22 01:11:07 +0900

    Initial Commit

---
+++ EspTouch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EspTouch
+//
+//  Created by Sang bo Hong on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EspTouch/EspTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EspTouchApp.swift
+//  EspTouch
+//
+//  Created by Sang bo Hong on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct EspTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
